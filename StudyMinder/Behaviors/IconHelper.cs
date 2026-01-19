cb9246b73467874e66c1f59438f20ed5dcb23cfb
Eric Jhon <ericjhon92@hotmail.com>
2026-01-19 20:06:27 -0300

    Sistema de icones nos campos de dados.

---
+++ StudyMinder/Behaviors/IconHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using MahApps.Metro.IconPacks;
+
+namespace StudyMinder.Behaviors
+{
+    public static class IconHelper
+    {
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.RegisterAttached(
+                "Icon",
+                typeof(PackIconMaterialKind),
+                typeof(IconHelper),
+                new PropertyMetadata(PackIconMaterialKind.None));
+
+        public static void SetIcon(DependencyObject element, PackIconMaterialKind value)
+            => element.SetValue(IconProperty, value);
+
+        public static PackIconMaterialKind GetIcon(DependencyObject element)
+            => (PackIconMaterialKind)element.GetValue(IconProperty);
+    }
+}
